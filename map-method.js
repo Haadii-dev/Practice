98fd14084c03a5dbe0e81a85bf7aa78661881e68
Hammad G. <125109241+Haadii-dev@users.noreply.github.com>
2026-02-15 23:16:28 +0000

    Create map-method.js
    Practice .map() method on arrays

---
+++ map-method.js
@@ -0,0 +1,20 @@
+// In this script, I will be practicing the map method in JavaScript. The map method is used to create a new array by applying a function to each element of an existing array. It does not modify the original array.
+
+// Lets say we have an array of numbers and we want to create a new array that contains the squares of those numbers:
+
+
+
+
+const numbers = [1, 2, 3, 4, 5]
+
+const squares = numbers.map(function(num){
+    return num * num
+})
+console.log(squares)
+
+// We can perform other functions too
+const a = 9
+const newArr = numbers.map(function(num){
+    return num * a
+})
+console.log(newArr)
